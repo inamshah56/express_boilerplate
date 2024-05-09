622f2cc7f8667f1be656db66e9bc588c66ebf1a4
unknown <gkazmi987@gmail.com>
2024-05-10 02:31:16 +0500

    updated utils->requiredFields.js

--- utils/requiredFields.js
+++ utils/requiredFields.js
@@ -2,7 +2,7 @@ const { validationError, frontError } = require("./responses");
 
 const queryReqFields = (req, res, field_list) => {
     for (const field of field_list) {
-        if (!req.query[field]) {
+        if (!req.query[field] || req.query[field].trim() == '') {
             return {
                 error: true,
                 resData: frontError(res, `${field}: This field is required.`),
@@ -14,7 +14,7 @@ const queryReqFields = (req, res, field_list) => {
 const bodyReqFields = (req, res, field_list) => {
     let resObj = {};
     for (const field of field_list) {
-        if (!req.body[field]) {
+        if (!req.body[field] || req.query[field].trim() == '') {
             resObj[[field]] = "This field is required.";
         }
     }
